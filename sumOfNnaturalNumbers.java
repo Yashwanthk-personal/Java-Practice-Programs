3ff1bfda14d5c20bb776762a548b67ee423d4856
iYashwanth007 <138038578+iYashwanth007@users.noreply.github.com>
2023-07-05 16:48:46 +0530

    Add files via upload

---
+++ sumOfNnaturalNumbers.java
@@ -0,0 +1,15 @@
+package Java_Package1;
+
+public class sumOfNnaturalNumbers {
+
+	public static void main(String[] args) {
+		
+		int n=555;
+		int sum=0;
+		for(int i=n;i>=1;i--)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
