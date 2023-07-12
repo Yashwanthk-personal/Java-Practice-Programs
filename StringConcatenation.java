6ed0ea28ea4595320f3c17a1f5944b376383709b
Yashwanth K <138038578+iYashwanth007@users.noreply.github.com>
2023-07-12 13:50:01 +0530

    adding two or more strings and storing those strings into the single variable

---
+++ StringConcatenation.java
@@ -0,0 +1,14 @@
+package String_Manipulation_Programs;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+
+		String firstName = "Yashwanth";
+		String lastName = "Krishna";
+		String fullName = firstName+" "+lastName;
+		System.out.println("The full of the user is "+ fullName);
+		
+	}
+
+}
