4937b929b87c29565b0f32f6d5237fa2d6985b20
Yashwanth K <138038578+iYashwanth007@users.noreply.github.com>
2023-07-12 13:46:39 +0530

    Split the provided String into the Equal parts as per your unputs

---
+++ SplitStringIntoEqualParts.java
@@ -0,0 +1,35 @@
+package String_Manipulation_Programs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitStringIntoEqualParts {
+
+	public static void main(String[] args) {
+         String str = "abcdefghijklmnopqrstuvwxyz";
+         int parts = 7;
+         
+         List<String> splitString = splitStringIntoParts(str,parts);
+         System.out.println(splitString);
+	}
+	
+	public static List<String> splitStringIntoParts(String str,int parts){
+		List<String> result = new ArrayList<>();
+		
+		int length = str.length();
+        int partLength = length/parts;
+        int remainingChar = length % parts;
+        int startIndex = 0;
+        
+        for(int i=0;i<parts;i++) {
+        	int currentPartLength = partLength + (remainingChar>0?1:0);
+        	String part = str.substring(startIndex, startIndex+currentPartLength);
+        	result.add(part);
+        	
+        	startIndex+=currentPartLength;
+        	remainingChar--;        	
+        }
+        return result;
+	}
+
+}
