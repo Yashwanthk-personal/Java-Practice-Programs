19fd6733ebaeb9e5e9628cf735d1244c2c6a9876
Yashwanth K <138038578+iYashwanth007@users.noreply.github.com>
2023-07-12 13:47:42 +0530

    Conversion of the given String case in to user specified case

---
+++ StringCaseConversion.java
@@ -0,0 +1,15 @@
+package String_Manipulation_Programs;
+
+public class StringCaseConversion {
+
+	public static void main(String[] args) {
+
+		String str = "Hello World*";
+		String lowerCase = str.toLowerCase();
+		String uppercase = str.toUpperCase();
+		
+		System.out.println("In lower case: "+lowerCase);
+		System.out.println("In upper case: "+uppercase);
+	}
+
+}
