572be432336ec225786884ff40b67bdc77c4788d
Yashwanth K <138038578+Yashwanthk-personal@users.noreply.github.com>
2023-09-26 20:27:47 +0530

    Corrected the indentation of the Whole code

--- SplitStringIntoEqualParts.java
+++ SplitStringIntoEqualParts.java
@@ -6,30 +6,28 @@ import java.util.List;
 public class SplitStringIntoEqualParts {
 
 	public static void main(String[] args) {
-         String str = "abcdefghijklmnopqrstuvwxyz";
-         int parts = 7;
+         	String str = "abcdefghijklmnopqrstuvwxyz";
+         	int parts = 7;
          
-         List<String> splitString = splitStringIntoParts(str,parts);
-         System.out.println(splitString);
+        	List<String> splitString = splitStringIntoParts(str,parts);
+         	System.out.println(splitString);
 	}
 	
 	public static List<String> splitStringIntoParts(String str,int parts){
 		List<String> result = new ArrayList<>();
-		
 		int length = str.length();
         	int partLength = length/parts;
         	int remainingChar = length % parts;
         	int startIndex = 0;
         
-        for(int i=0;i<parts;i++) {
-        	int currentPartLength = partLength + (remainingChar>0?1:0);
-        	String part = str.substring(startIndex, startIndex+currentPartLength);
-        	result.add(part);
+       		for(int i=0;i<parts;i++) {
+        		int currentPartLength = partLength + (remainingChar>0?1:0);
+        		String part = str.substring(startIndex, startIndex+currentPartLength);
+        		result.add(part);
         	
-        	startIndex+=currentPartLength;
-        	remainingChar--;        	
-        }
+        		startIndex += currentPartLength;
+        		remainingChar--;        	
+        	}
         return result;
-	}
-
+        }
 }
